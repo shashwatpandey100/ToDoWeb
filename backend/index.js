902c1ea03ea9b2bc61bdcdbff66e772d81cb93f1
shashwatpandey100 <115147038+shashwatpandey100@users.noreply.github.com>
2023-10-28 22:04:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(cors(
     {
-        origin: ["https://to-do-web-rho.vercel.app/"],
+        origin: ["https://to-do-web-rho.vercel.app"],
         methods: ["*"],
         credentials: true
     }
