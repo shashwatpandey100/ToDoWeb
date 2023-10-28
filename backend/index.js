b17b793367243de2ef04373b6fa3534f0b9a6769
shashwatpandey100 <115147038+shashwatpandey100@users.noreply.github.com>
2023-10-28 21:48:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,13 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://to-do-web-rho.vercel.app/"],
+        methods: ["*"],
+        credentials: true
+    }
+));
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser());
